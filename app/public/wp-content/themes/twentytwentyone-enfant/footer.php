7859a2480f1306e42c2914fdf5ba5d1bfc23ef4b
nikosvdh <85417907+nikosvdh@users.noreply.github.com>
2023-02-20 16:27:41 +0100

    Add header and footer

---
+++ app/public/wp-content/themes/twentytwentyone-enfant/footer.php
@@ -0,0 +1,15 @@
+ </main>
+ </div>
+ <footer id="footer">
+
+     <nav class="margin">
+         <?php
+ wp_nav_menu ( array ('theme_location' => 'footer-menu') ); ?>
+     </nav>
+
+ </footer>
+ </div>
+ <?php wp_footer(); ?>
+ </body>
+
+ </html>
