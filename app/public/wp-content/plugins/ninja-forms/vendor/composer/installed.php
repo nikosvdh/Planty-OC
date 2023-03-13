a01cdd92284ced8bbaf6966a0875f2a873eba306
nikosvdhgit config --global user.name nikosvdhgit config --global user.name nikosvdh <nikolavdh@gmail.com>
2023-03-13 22:20:17 +0100

    first commit media queries order page

---
+++ app/public/wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-7a8400703b4afe81bf23553fbe4c3bfe2c9a6fa9',
+        'version' => 'dev-7a8400703b4afe81bf23553fbe4c3bfe2c9a6fa9',
+        'reference' => '7a8400703b4afe81bf23553fbe4c3bfe2c9a6fa9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-7a8400703b4afe81bf23553fbe4c3bfe2c9a6fa9',
+            'version' => 'dev-7a8400703b4afe81bf23553fbe4c3bfe2c9a6fa9',
+            'reference' => '7a8400703b4afe81bf23553fbe4c3bfe2c9a6fa9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
